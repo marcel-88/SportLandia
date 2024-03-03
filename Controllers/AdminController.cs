a1695498f893e35c6373a22dee7462218e95af54
Vovbl4 <odainikv@gmail.com>
2024-03-03 20:17:58 +0200

    Adding Admin Pannel

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TW_WebSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminChangeProducts()
+        {
+            return View();
+        }
+    }
+}
--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,4 +15,4 @@ namespace TW_WebSite.Models
         public DbSet<Users> Products { get; set; }
         
     }
-}
+}*/
