ec99efe724db16024e7958c66b48a8bb382e44f6
Vovbl4 <odainikv@gmail.com>
2024-03-03 22:11:54 +0200

    Adding BL

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using eUseControl.BusinessLogic;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.User;
+
+namespace TW_WebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ISession _session;
+
+        public LoginController()
+        {
+            var bl = new BusinessLogic();
+            _session = bl.GetSessionBL();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(UserLogin login)
+        {
+            if (ModelState.IsValid)
+            {
+                ULoginData data = new ULoginData()
+                {
+                    Credential = login.Credential,
+                    Password = login.Password,
+                    LoginIP = Request.UserHostAddress,
+                    LoginDateTime = DateTime.Now,
+                };
+
+                var userLogin = _session.UserLogin(data);
+                if(userLogin.Status)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", userLogin.StatusMsg);
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
+/*        public ActionResult Register()
+        {
+            return View();
+        }*/
+    }
+
+}
