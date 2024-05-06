ec63cba36d13442b248b15de953e97f80aac3dfe
Vovbl4 <odainikv@gmail.com>
2024-05-06 23:22:49 +0300

    Creating reset password option is user's profile

--- SportLandia/Controllers/HomeController.cs
+++ SportLandia/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eUseControl.BusinessLogic;
+using eUseControl.BusinessLogic.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,14 @@ namespace TW_WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISession _session;
+
+        public HomeController()
+        {
+            var bl = new BusinessLogic();
+            _session = bl.GetSessionBL();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -32,5 +42,62 @@ namespace TW_WebSite.Controllers
         {
             return View();
         }
+
+        public ActionResult UProfile()
+        {
+            var cookie = Request.Cookies["X-KEY"].Value;
+            if (cookie != null)
+            {
+                var session = _session.GetUserByCookie(cookie);
+                if (session != null)  // && session.ExpireTime > DateTime.Now
+                {
+                    ViewBag.Username = session.Username;
+                    return View(new PasswordChangeModel());
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("Session Expired");
+                    TempData["Error"] = "Session expired. Please log in again.";
+                    return RedirectToAction("Login", "Auth");
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("No session found");
+                TempData["Error"] = "No active session found. Please log in.";
+                return RedirectToAction("Login", "Auth");
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangePassword(PasswordChangeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = HttpContext.User.Identity.Name;
+            if (user == null)
+            {
+                ModelState.AddModelError("", "User not recognized.");
+                return View(model);
+            }
+
+            bool result = _session.ChangeUserPassword(user, model.OldPassword, model.NewPassword);
+            if (result)
+            {
+                TempData["Message"] = "Password successfully updated.";
+                return RedirectToAction("UProfile");
+            }
+            else
+            {
+                ModelState.AddModelError("", "The old password is incorrect or update failed.");
+                return View(model);
+            }
+        }
+
     }
 }
