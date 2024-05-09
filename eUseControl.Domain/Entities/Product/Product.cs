67a9f0f30a5414e628d595fcc4f129c47932838a
AthleteGOAT <max.pein.14@mail.ru>
2024-05-09 08:39:06 +0300

    fix image path null

--- eUseControl.Domain/Entities/Product/Product.cs
+++ eUseControl.Domain/Entities/Product/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,17 @@ namespace eUseControl.Domain.Entities.Product
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public virtual Category Category { get; set; }
+        [Required]
         public string ImagePath { get; set; }
     }
 
