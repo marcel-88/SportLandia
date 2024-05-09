8f605ca80b4d98e480963fbcc05aad1bc319c8d6
AthleteGOAT <max.pein.14@mail.ru>
2024-05-10 00:14:36 +0300

    last fix product, cannot create product

---
+++ eUseControl.BusinessLogic/Migrations/202405092049199_FourthMigration.cs
@@ -0,0 +1,22 @@
+﻿namespace eUseControl.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FourthMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Description", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "ImagePath", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "ImagePath", c => c.String());
+            AlterColumn("dbo.Products", "Description", c => c.String());
+            AlterColumn("dbo.Products", "Name", c => c.String());
+        }
+    }
+}
